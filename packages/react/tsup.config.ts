e14bb29204bc257f5afd4b5a7ac35241d17b8fcb
Oc1S <ocis.chan@gmail.com>
2024-08-29 00:49:32 +0800

    chore: publish

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig(options => ({
-  entry: ['./src/index.tsx'],
-  format: ['cjs', 'esm'],
-  minify: !options.watch,
-  splitting: false,
-  sourcemap: true,
-  clean: false,
-  dts: true,
-  external: ['react', 'react-dom'],
-}));
+export default defineConfig(options => {
+  const isDev = !!options.watch;
+  return {
+    entry: ['./src/index.tsx'],
+    format: ['cjs', 'esm'],
+    minify: !isDev,
+    splitting: false,
+    sourcemap: isDev,
+    clean: !isDev,
+    dts: true,
+    external: ['react', 'react-dom'],
+  };
+});
