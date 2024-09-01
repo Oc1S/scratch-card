5e442497f5f1d9f96c44516c9ad5d9b419dd8819
Oc1S <ocis.chan@gmail.com>
2024-09-01 22:47:33 +0800

    ci: release

---
+++ .releaserc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  branches: ['release'], // 指定在哪个分支下要执行发布操作
+  plugins: [
+    '@semantic-release/commit-analyzer', // 解析 commit 信息，默认就是 Angular 规范
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md', // 把发布日志写入该文件
+      },
+    ],
+    '@semantic-release/npm', // 发布到NPM
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'], // 日志记录和版本号是新增修改的，需要 push 回 Git
+      },
+    ],
+  ],
+};
